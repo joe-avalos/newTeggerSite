e7531ec68f5ae411abceafbb9276db25d1a5efb0
joe-avalos <jose.avalos@culturacolectiva.com>
2019-07-26 19:32:19 -0500

    removed dummy items actions and reducer

--- src/modules/reducers/index.js
+++ src/modules/reducers/index.js
@@ -1,12 +1,10 @@
 import {combineReducers} from 'redux'
 import {connectRouter} from "connected-react-router"
-import {itemsReduce} from "./items"
 import {userReduce} from './userReducer'
 import {loggedReduce} from './loggedReducer'
 
 export default history => combineReducers({
   router: connectRouter(history),
-  items: itemsReduce,
   user: userReduce,
   logged: loggedReduce
 })
